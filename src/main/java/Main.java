f3c01fdd468ba0d417c4642b9ae3defd8bc4fbd0
coconutnutX <coconutnutxs@gmail.com>
2022-04-03 16:07:38 +0200

    add build

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,21 +2,33 @@ import java.util.List;
 
 import mips.Control;
 import mips.Instruction;
+import mips.state.CM;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import util.Parser;
 
 public class Main {
+
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        String testFileName = args[0];
+        logger.info("input file: " + testFileName);
+
         // read and parse instruction
-        List<Instruction> instructions = Parser.readInstruction("test6.json");
+        List<Instruction> instructions = Parser.readInstruction(testFileName);
 
         // initialize control
         Control control = new Control(instructions);
 
+        logger.info("start simulating...");
         while(control.isPropagating){
             control.propagate();
         }
 
         // dump JSON
-        Parser.outputJSON(control.storageList, "test6_output.json");
+        String outputFileName = testFileName.split("\\.")[0] + "_output.json";
+        logger.info("output file: " + outputFileName);
+        Parser.outputJSON(control.storageList, outputFileName);
     }
 }
